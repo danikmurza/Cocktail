ed89a581dce626ae1b5913c3b59111fafca9580d
Daniyar Murzaguzhinov <daniyarmurza@gmail.com>
2021-10-07 18:44:40 -0500

    showLoading

--- coctail/src/presentDrinks.js
+++ coctail/src/presentDrinks.js
@@ -1,12 +1,12 @@
 import fetchDrinks from "./fetchDrinks.js";
 import displayDrinks from "./displayDrinks.js";
-import setDrink from "./setDrink.js";
+import {setCocktail} from "./service.js";
 
 const showDrinks = async (url) => {
   const data = await fetchDrinks(url);
   const section = await displayDrinks(data);
   if (section) {
-    setDrink(section);
+    setCocktail(section);
   }
 };
 
